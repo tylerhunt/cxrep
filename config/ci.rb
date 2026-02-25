5ea41470e488dfd89bff95874a7cef0bd8ca8698
Tyler Hunt <tyler@tylerhunt.com>
2026-02-25 14:34:30 -0500

    Run RSpec with bin/ci

--- config/ci.rb
+++ config/ci.rb
@@ -11,6 +11,8 @@ CI.run do
   step 'Security: Brakeman code analysis',
     'bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error'
 
+  step 'Test: RSpec', 'bundle exec rspec'
+
   # Optional: set a green GitHub commit status to unblock PR merge.
   # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
   # if success?
